0fc376b65c22ba97e6485005ffe2dd777fe8cd33
Seth Alley <salley32@byu.edu>
2024-04-08 15:19:04 -0600

    Add project files.

---
+++ INTEX_AURORA_BRICKS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INTEX_AURORA_BRICKS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
