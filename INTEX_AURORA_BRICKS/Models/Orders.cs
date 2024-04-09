5fd0dad0dccd18081078e4f61683323d2a510bbf
John Doe <johndoe@example.com>
2024-04-09 10:03:31 -0600

    Pushing the Models and context files

---
+++ INTEX_AURORA_BRICKS/Models/Orders.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace INTEX_AURORA_BRICKS.Models
+{
+    public class Order
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        [Display(Name = "Day of Week")]
+        public string? DayOfWeek { get; set; }
+
+        public int? Time { get; set; }
+
+        [Display(Name = "Entry Mode")]
+        public string? EntryMode { get; set; }
+
+        public int? Amount { get; set; }
+
+        [Display(Name = "Type of Transaction")]
+        public string? TypeOfTransaction { get; set; }
+
+        [Display(Name = "Country of Transaction")]
+        public string? CountryOfTransaction { get; set; }
+
+        [Display(Name = "Shipping Address")]
+        public string? ShippingAddress { get; set; }
+
+        public string? Bank { get; set; }
+
+        [Display(Name = "Type of Card")]
+        public string? TypeOfCard { get; set; }
+
+        public bool? Fraud { get; set; }
+
+        // Navigation property
+        public virtual Customer Customer { get; set; }
+    }
+}
---
+++ INTEX_AURORA_BRICKS/Models/Products.cs
@@ -0,0 +1,29 @@
+﻿namespace INTEX_AURORA_BRICKS.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        public string? Name { get; set; }
+
+        public int? Year { get; set; }
+
+        public int? NumParts { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public string? ImgLink { get; set; }
+
+        public string? PrimaryColor { get; set; }
+
+        public string? SecondaryColor { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Category { get; set; }
+    }
+
+}
