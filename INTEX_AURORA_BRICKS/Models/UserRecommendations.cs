d49131ee140a1e512f0386eca903627aab896350
Seth Alley <salley32@byu.edu>
2024-04-10 19:15:10 -0600

    user recommendation models

---
+++ INTEX_AURORA_BRICKS/Models/UserRecommendations.cs
@@ -0,0 +1,13 @@
+﻿namespace INTEX_AURORA_BRICKS.Models
+{
+    public class UserRecommendations
+    {
+        public int customer_ID { get; set; }
+        public int Recommendation_1_ID { get; set; }
+        public int Recommendation_2_ID { get; set; }
+        public int Recommendation_3_ID { get; set; }
+        public int Recommendation_4_ID { get; set; }
+        public int Recommendation_5_ID { get; set; }
+
+    }
+}
